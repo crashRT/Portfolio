57a39ee0dd7ec076465c75a765f2b65db0a639d9
crashRT <85057127+crashRT@users.noreply.github.com>
2021-09-15 06:00:37 +0900

    Picture複数枚OKにした
    ImageField6個作って最大6枚まで画像投稿できるようにした

--- portfolio/picture/models.py
+++ portfolio/picture/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class PictureModel(models.Model):
-    picture = models.ImageField(upload_to='picture')
     title = models.CharField(max_length=200)
+    picture1 = models.ImageField(upload_to='picture', null = True, blank = True, default = '')
+    picture2 = models.ImageField(upload_to='picture', null = True, blank = True, default = '')
+    picture3 = models.ImageField(upload_to='picture', null = True, blank = True, default = '')
+    picture4 = models.ImageField(upload_to='picture', null = True, blank = True, default = '')
+    picture5 = models.ImageField(upload_to='picture', null = True, blank = True, default = '')
+    picture6 = models.ImageField(upload_to='picture', null = True, blank = True, default = '')
     discription = models.CharField(max_length=1000)
 
     def __str__(self):
