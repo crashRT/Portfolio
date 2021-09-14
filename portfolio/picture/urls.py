ab0c37515bb1f36aadd6919238ca514e7465db83
crashRT <85057127+crashRT@users.noreply.github.com>
2021-09-15 04:53:13 +0900

    PictureDetailBase
    Pictureの詳細画面でも画像表示できるようにした

--- portfolio/picture/urls.py
+++ portfolio/picture/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
-from .views import PictureList
+from .views import PictureList, PictureDetail
 
 urlpatterns = [
     path('list/', PictureList.as_view()),
+    path('detail/<int:pk>/',PictureDetail.as_view()),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- portfolio/picture/views.py
+++ portfolio/picture/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import PictureModel
 
 class PictureList(ListView):
     template_name = 'PictureList.html'
+    model = PictureModel
+
+class PictureDetail(DetailView):
+    template_name = 'PictureDetail.html'
     model = PictureModel
