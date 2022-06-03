c84b949a55dfbbf68e93d25b31236f07635841fa
crashRT <85057127+crashRT@users.noreply.github.com>
2022-06-03 10:32:33 +0900

    modelにdateを追加, note→urlへ変更

--- portfolio/note/models.py
+++ portfolio/note/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
 class NoteModel(models.Model):
     title = models.CharField(max_length=200)
+    date = models.DateTimeField()
     category = models.CharField(max_length=200)
     abstract = models.CharField(max_length=200, null=True, blank=True)
-    note = models.TextField(null=True, blank=True)
+    url = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
