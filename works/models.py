ea27a31ca113213bbd83de5a2b69e4aee98f1ca4
crashRT <85057127+crashRT@users.noreply.github.com>
2022-08-12 00:52:00 +0900

    モデルから画像を表示できるように

--- works/models.py
+++ works/models.py
@@ -11,15 +11,18 @@ CATEGORY = (('pictre', 'Picture'),
 
 class WorksModel(models.Model):
     title = models.CharField(max_length=50)
-    thumbnail = models.TextField()
-    youtube = models.TextField(null=True, blank=True)
-    vimeo = models.TextField(null=True, blank=True)
-    niconico = models.TextField(null=True, blank=True)
-    picture1 = models.TextField(null=True, blank=True)
-    picture2 = models.TextField(null=True, blank=True)
-    picture3 = models.TextField(null=True, blank=True)
-    picture4 = models.TextField(null=True, blank=True)
-    picture5 = models.TextField(null=True, blank=True)
-    picture6 = models.TextField(null=True, blank=True)
+    thumbnail = models.CharField(max_length=100,)
+    youtube = models.CharField(max_length=100, null=True, blank=True)
+    vimeo = models.CharField(max_length=100, null=True, blank=True)
+    niconico = models.CharField(max_length=100, null=True, blank=True)
+    picture1 = models.CharField(max_length=100, null=True, blank=True)
+    picture2 = models.CharField(max_length=100, null=True, blank=True)
+    picture3 = models.CharField(max_length=100, null=True, blank=True)
+    picture4 = models.CharField(max_length=100, null=True, blank=True)
+    picture5 = models.CharField(max_length=100, null=True, blank=True)
+    picture6 = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     tag = models.CharField(max_length=30, choices=CATEGORY)
+
+    def __str__(self):
+        return self.title
