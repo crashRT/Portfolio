aa357a71d0519b1fa1017ca092bcd779d11bafbe
crashRT <85057127+crashRT@users.noreply.github.com>
2022-08-12 00:12:09 +0900

    画像を表示できるようにした

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -113,6 +113,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
