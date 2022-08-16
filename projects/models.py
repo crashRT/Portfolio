2bee7b3a1dc29ab308d2ce58dd1c73ed2b329df7
crashRT <crashrt.farbauti@gmail.com>
2022-08-16 14:03:24 +0900

    model に pictureMain 追加など

--- projects/models.py
+++ projects/models.py
@@ -8,11 +8,15 @@ CATEGORY = (('aescript', 'aescript'),
             )
 
 tags = ('web', )
+
+
 class ProjectsModel(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(blank=True, null=True)
     thumbnail = models.CharField(max_length=100)
     abstract = models.TextField()
+    pictureMain = models.CharField(
+        max_length=100, null=True, blank=True, help_text='一番上に単体で表示')
     picture1 = models.CharField(
         max_length=100, null=True, blank=True, help_text='以下はギャラリー部分')
     picture2 = models.CharField(max_length=100, null=True, blank=True)
@@ -21,7 +25,8 @@ class ProjectsModel(models.Model):
     picture5 = models.CharField(max_length=100, null=True, blank=True)
     picture6 = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    tag = models.CharField(max_length=30, choices=CATEGORY)
+    tag = models.CharField(
+        max_length=30, choices=CATEGORY, null=True, blank=True)
 
     def __str__(self):
         return self.title
