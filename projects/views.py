0171f6b755b739ecff3dd04d4f880c2acafc5b86
crashRT <crashrt.farbauti@gmail.com>
2022-08-16 14:04:11 +0900

    rename

--- projects/views.py
+++ projects/views.py
@@ -3,22 +3,25 @@ from .models import ProjectsModel, tags
 
 # Create your views here.
 
+
 def projectslistview(request):
     projects_list = ProjectsModel.objects.all()
     context = {
-        'projects_list':projects_list,
-        'taglist':tags
+        'projects_list': projects_list,
+        'taglist': tags
     }
     return render(request, 'projects/list.html', context)
 
-def projectdetailview(request, pk):
-    project_list = ProjectsModel.objects.get(pk=pk)
-    return render(request, 'projects/detail.html', {'project_list': project_list})
 
-def workstagview(requested, tagname):
+def projectsdetailview(request, pk):
+    project = ProjectsModel.objects.get(pk=pk)
+    return render(request, 'projects/detail.html', {'project': project})
+
+
+def projectstagview(requested, tagname):
     projects_list = ProjectsModel.objects.filter(tag__in=[tagname]).distinct()
     context = {
         'projects_list': projects_list,
         'taglist': tags,
     }
-    return render(requested, 'works/list.html', context)
+    return render(requested, 'projects/list.html', context)
