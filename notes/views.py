cac0dfef37ac230198597609bd837ed85fdbb5c1
crashRT <crashrt.farbauti@gmail.com>
2022-08-17 00:28:15 +0900

    notes view

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import NotesModel, tags
 
 # Create your views here.
+
+
+def noteslistview(request):
+    notes_list = NotesModel.objects.all()
+    context = {
+        'notes_list': notes_list,
+        'taglist': tags
+    }
+    return render(request, 'notes/list.html', context)
+
+
+def notestagview(requested, tagname):
+    notes_list = NotesModel.objects.filter(tag__in=[tagname]).distinct()
+    context = {
+        'notes_list': notes_list,
+        'taglist': tags,
+    }
+    return render(requested, 'notes/list.html', context)
