397dc8888f80711f259c43d08565115de4eee108
crashRT <crashrt.farbauti@gmail.com>
2022-08-17 00:25:35 +0900

    set urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('works/', include('works.urls')),
     path('projects/', include('projects.urls')),
+    path('notes/', include('notes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
