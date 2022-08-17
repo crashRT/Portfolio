8745546b36195869f1be707d45b39404f2fd9d0b
crashRT <crashrt.farbauti@gmail.com>
2022-08-18 02:16:02 +0900

    デプロイ用の設定に

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,9 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crashrt.work', 'www.crashrt.work',
+                 'localhost', 'portfolio-335223.an.r.appspot.com']
 
 
 # Application definition
@@ -114,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/staticfile/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files
