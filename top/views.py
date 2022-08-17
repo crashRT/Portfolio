6c15378d393bab14385110af748327a3e436d1d5
crashRT <crashrt.farbauti@gmail.com>
2022-08-17 12:48:29 +0900

    rename view

--- top/views.py
+++ top/views.py
@@ -6,13 +6,13 @@ from notes.models import NotesModel
 # Create your views here.
 
 
-def worklistview(request):
+def topview(request):
     works_list = WorksModel.objects.all()[:6]
     projects_list = ProjectsModel.objects.all()[:3]
-    notes_list = NotesModel.objects.all()[:3]
+    notes_list = NotesModel.objects.all()[:4]
     context = {
         'works_list': works_list,
         'projects_list': projects_list,
         'notes_list': notes_list,
     }
-    return render(request, 'index.html', context)
+    return render(request, 'top/index.html', context)
