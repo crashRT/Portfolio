1402ed024ea2e9d032eb889f308ea20df38ba4db
crashRT <crashrt.farbauti@gmail.com>
2022-08-17 11:21:40 +0900

    view

--- top/views.py
+++ top/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from works.models import WorksModel
+from projects.models import ProjectsModel
+from notes.models import NotesModel
 
 # Create your views here.
+
+
+def worklistview(request):
+    works_list = WorksModel.objects.all()[:6]
+    projects_list = ProjectsModel.objects.all()[:3]
+    notes_list = NotesModel.objects.all()
+    context = {
+        'works_list': works_list,
+        'projects_list': projects_list,
+        'notes_list': notes_list,
+    }
+    return render(request, 'index.html', context)
